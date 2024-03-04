3a6ca398c32e7e9b9fc63b28b59a03f6edcec930
ssabirovo <muhiddinsobirov957@gmail.com>
2024-03-04 12:43:35 +0500

    fix: language bugs are fixed

--- src/services/i18n/i18n.tsx
+++ src/services/i18n/i18n.tsx
@@ -4,13 +4,31 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+import translationEN from "../../../public/locales/EN.json";
+import translationUz from "../../../public/locales/UZ.json";
+import translationTR from "../../../public/locales/TR.json";
+import translationRU from "../../../public/locales/RU.json";
+
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    backend: { loadPath: "/locales/{{lng}}.json" },
-    fallbackLng: "TR",
+    lng: "en",
+    fallbackLng: "en",
+    resources: {
+      en: {
+        translation: translationEN,
+      },
+      uz: {
+        translation: translationUz,
+      },
+      tr: {
+        translation: translationTR,
+      },
+      ru: {
+        translation: translationRU,
+      },
+    },
     detection: {
       order: ["localStorage", "htmlTag", "path", "subdomain"],
       caches: ["localStorage"],
