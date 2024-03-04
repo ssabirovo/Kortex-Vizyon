68b44d088aceb84a6bc00dddf4304974e4f184d8
ssabirovo <muhiddinsobirov957@gmail.com>
2024-03-04 12:48:32 +0500

    chore: update

--- src/services/i18n/i18n.tsx
+++ src/services/i18n/i18n.tsx
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 import translationEN from "../../../public/locales/EN.json";
@@ -9,6 +10,7 @@ import translationTR from "../../../public/locales/TR.json";
 import translationRU from "../../../public/locales/RU.json";
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
